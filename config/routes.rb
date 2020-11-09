1a4436b01572f3094b3f7da786f60b4f737f4f70
Travis Mix <travismix1980@gmail.com>
2020-11-09 08:47:50 -0800

    added our first two static pages with simple html

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'home',to: 'static_pages#home'
-  get 'help', to: 'static_pages#help'
+  get 'static_pages/home'
+  get 'static_pages/help'
   root 'application#hello'
 end
