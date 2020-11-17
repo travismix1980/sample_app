e5194b3a5c36a791d107bfef870f3d9d3e45e36a
Travis Mix <travismix1980@gmail.com>
2020-11-17 14:33:20 -0800

    various small changes to tests and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@
 
 # User model
 class User < ApplicationRecord
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -51,4 +51,11 @@ foo@bar..com)
     @user.save
     assert_not duplicate_user.valid?
   end
+
+  test 'email addresses should be saved as lower-case' do
+    mixed_case_email = 'Foo@ExAMPle.CoM'
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
 end
